0f0577ea04bf35e6a7ee48d6aa9515fb27009077
Bartosz Piwek <bartoszpiwek@gmail.com>
2020-04-02 15:54:59 +0200

    Example can have html code

---
+++ dist/helpers/ifOr.js
@@ -0,0 +1,8 @@
+module.exports = function(Handlebars) {
+  Handlebars.registerHelper('ifOr', function(v1, v2, options) {
+    if (v1 || v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+};
