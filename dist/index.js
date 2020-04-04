4926cd5a0865d887ff2c4c558e9f48b4357e81bf
bpiwek <bartosz.piwek@gogomedia.pl>
2020-04-04 11:55:15 +0200

    Working

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+const KssBuilderHandlebars = require("kss/builder/handlebars");
+
+class FrontboxKSSSlyleguide extends KssBuilderHandlebars {
+  constructor() {
+    super();
+  }
+
+  prepare(styleGuide) {
+    return super.prepare(styleGuide).then((styleGuide) => {
+      require("./helpers/prism")(this.Handlebars);
+      return styleGuide;
+    });
+  }
+}
+
+module.exports = FrontboxKSSSlyleguide;
